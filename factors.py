a3308fd50b38b77c66fe62ae23937646d61eb734
Forster112 <forstermichael112@gmail.com>
2022-06-22 16:15:38 +0100

    found this

--- factors.py
+++ factors.py
@@ -1,10 +1,29 @@
-def factors(num):
-    fac_num = []
-    while num > 1:
-        for i in range(1, num):
-            if num % i == 0:
-                fac_num.append(i)
-        break
-    print("{}={}*{}".format(num, fac_num[-1], fac_num[1]))
+#!/usr/bin/python3
+from sys import argv
 
-factors(2398)
+def mult_arr(arr):
+    prod = 1
+    for x in arr:
+        prod *= x
+    return prod
+
+def factorializer(n):
+    primefactors = []
+    d = 2
+    while d * d <= n:
+        while (n % d) == 0:
+            primefactors.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+       primefactors.append(n)
+    q = primefactors.pop(0)
+    return (mult_arr(primefactors), q)
+
+def main():
+    with open(argv[1]) as f:
+        for line in f:
+            print('{}={}*{}'.format(line.rstrip('\n'), *factorializer(int(line))))
+
+if __name__ == '__main__':
+    main()
